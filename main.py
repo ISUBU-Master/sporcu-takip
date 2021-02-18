3c33d72b29e1e5d945cfbb6d15340521a81d7188
Furkan Çelik <furkancelik@Furkan-MacBook-Pro.local>
2021-02-19 02:50:07 +0300

    main page create window

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
+from HomeLayoutUI import *
+
+
+application = QApplication(sys.argv)
+mainWindow=QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(mainWindow)
+mainWindow.show()
+
+
+
+sys.exit(application.exec_())
